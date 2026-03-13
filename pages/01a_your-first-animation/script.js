19d0edf9a6eb1bf45990dc4ae1165f56484e40e3
thamizh-root <58970160+thamizh-root@users.noreply.github.com>
2026-03-13 13:45:36 +0530

    latest

--- pages/01a_your-first-animation/script.js
+++ pages/01a_your-first-animation/script.js
@@ -1,13 +1,24 @@
+// gsap.to(".card", {
+//   opacity: 1,
+//   scale: 1,
+//   duration: 5,
+//   onComplete: () => {
+//     gsap.to(".card", {
+//       y: -20,
+//       repeat: -1,
+//       yoyo: true,
+//       duration: 0.5,
+//     });
+//   },
+// });
+
+import gsap from "gsap";
+
 gsap.to(".card", {
   opacity: 1,
-  scale: 1,
-  duration: 5,
-  onComplete: () => {
-    gsap.to(".card", {
-      y: -20,
-      repeat: -1,
-      yoyo: true,
-      duration: 0.5,
-    });
-  },
+  duration: 8,
+  rotation: 360,
+  yoyo: true,
+  repeat: -1,
+  scale: 1.2
 });
--- pages/02a_understanding-properties/script.js
+++ pages/02a_understanding-properties/script.js
@@ -1,16 +1,35 @@
 import gsap from "gsap";
 
+// const repeat = document.querySelector(".repeat");
+
+// const animation = gsap.to(".box", {
+//   opacity: 1,
+//   rotation: 360,
+//   backgroundColor: "#ff6f61",
+//   borderRadius: "50%",
+//   scale: 1.25,
+//   duration: 2,
+// });
+
+// repeat.addEventListener("click", () => {
+//   animation.restart();
+// });
+
+
 const repeat = document.querySelector(".repeat");
 
 const animation = gsap.to(".box", {
-  opacity: 1,
-  rotation: 360,
-  backgroundColor: "#ff6f61",
-  borderRadius: "50%",
-  scale: 1.25,
-  duration: 2,
+      opacity: 1,
+      duration: 5,
+      rotation: 360,
+      scale: 1.2,
+      x: 200,
+      yoyo: true,
+      repeat: -1
 });
 
 repeat.addEventListener("click", () => {
   animation.restart();
 });
+
+
